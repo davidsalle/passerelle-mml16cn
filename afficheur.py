d075c4b9bb0b97d105b972ec64cb379e7afaa1f7
David Sallé <david.salle@ensemblescolaire-niort.com>
2025-12-06 14:47:35 +0100

    Ajout serveur passerelle

--- afficheur.py
+++ afficheur.py
@@ -7,8 +7,9 @@ import serial
 class Afficheur:
     """Classe pour gérer le journal lumineux MML16CN"""
 
-    def __init__(self, port_serie="COM3"):
+    def __init__(self, port_serie="COM3", numero=0):
         """Initialisation de l'afficheur"""
+        self.numero = numero
         try:
             self.liaison = serial.Serial(
                 port_serie,
@@ -23,18 +24,24 @@ class Afficheur:
         except serial.SerialException as erreur:
             traceback.print_exc()
             self.liaison = None
+
         
     def __del__(self):
         """A la fermeture"""
         if self.liaison is not None:
             self.liaison.close()
 
+    def calculer_checksum(self, donnees: str) -> str:
+        """Calcule la somme de contrôle (checksum) d'une trame de données"""
+        checksum = 0x00                     # Initialisation de la somme de contrôle
+        for caractere in donnees:           # Pour chaque caractère de la trame
+            checksum ^= ord(caractere)      # XOR du code ASCII du caractère
+        return  f"{checksum:02X}"           # Mise en forme sur 2 digits en majuscules
+    
     def preparer_trame(self, donnees) -> str:
         """Prépare une trame <IDxx>...yy<E>"""
-        checksum = 0x00
-        for caractere in donnees:
-            checksum ^= ord(caractere)
-        trame = f"<ID00>{donnees}{checksum:02X}<E>"
+        checksum = self.calculer_checksum(donnees)       
+        trame = f"<ID{self.numero:02d}>{donnees}{checksum}<E>"
         return trame
 
     def envoyer(self, trame) -> str:
@@ -49,12 +56,12 @@ class Afficheur:
     def mettre_a_jour(self, parametres, texte) -> str:
         """Met à jour l'affichage sur l'afficheur"""
         trame = f"{parametres}{texte}"
-        requete = f"<ID00><BE>05<E>{self.preparer_trame(trame)}<ID00><BF>06<E>"
+        requete = f"{self.preparer_trame("<BE>")}{self.preparer_trame(trame)}{self.preparer_trame("<BF>")}"
         self.envoyer(requete)
 
 
 if __name__ == "__main__":
-    a = Afficheur("COM3")
+    a = Afficheur("COM3", 0)
     #message = [0x01, 0x03] + [ord(c) for c in "Bonjour"] + [0x0A]
     #message = "<ID00><BE>05<E><ID00><L1><PA><FE><MA><WC><FE>bonjour15<E><ID00><BF>06<E>"
     #message = "<ID01><BE>05<E><ID01><L1><PA><FE><MA><WC><FE>bonjour15<E><ID01><BF>06<E>"
@@ -63,12 +70,13 @@ if __name__ == "__main__":
     #print(a.preparer_trame("<BE>"))
     #print(a.preparer_trame("<BF>"))
     #print(a.preparer_trame("<L1><PA><FE><MA><WC><FE>bonkour"))
-    #message = "<ID00><BQ>11<E><ID00><L1><PA><FE><MA><WC><FE>bonjour15<E>"
+    #message = "<ID03><BE>05<E><ID03><L1><PA><FE><MQ><WC><FA><CA>Hello world60<E><ID03><BF>06<E>"
     #print(a.envoyer(message))
 
     #a.envoyer(a.preparer_trame("Salut"))
 
-    a.mettre_a_jour("<L1><PA><FE><MQ><WC><FA>", "<CA>Sortie <U26>")
+    #a.mettre_a_jour("<L1><PA><FE><MQ><WC><FA>", "<CA>Hello world")
+    a.mettre_a_jour("<L1><PA><FL><MA><WB><FF>", "<CD>Johnny")
 
 
 
---
+++ main.py
@@ -0,0 +1,26 @@
+# Librairie(s) utilisée(s)
+from flask import Flask, request
+from afficheur import Afficheur
+
+
+# Créations des objets
+app = Flask(__name__)
+aff = None
+
+# Préparation des routes
+@app.route("/api", methods=["POST"])
+def envoyer_trame_brute():
+    """Récupère une trame brute et la fait suivre vers l'afficheur via une liaison série"""
+    global aff
+    trame = request.form.get("trame")
+    if trame is not None:
+        reponse = aff.envoyer(trame)
+        return "OK"
+    else:
+        return "NOK"
+
+
+# Lancement
+if __name__ == "__main__":
+    aff = Afficheur("COM3")
+    app.run(host="0.0.0.0", debug=True, use_reloader=False)
